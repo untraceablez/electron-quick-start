e0e60578b6d3d844492bc45d9c8da50070a8127b
Taylor Cohron <untraceablez@users.noreply.github.com>
2020-03-25 09:37:44 -0500

    Create learn.js

---
+++ learn.js
@@ -0,0 +1,12 @@
+//learn.js
+//This is just a playground for me to learn JS, new programmer here.
+
+//Section 1
+var myName = "Taylor";
+var myBday = "29";
+
+let companyName = "DePaul";
+
+const pi = 3.14;
+
+console.log myName
